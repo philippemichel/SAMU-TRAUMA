6a61fad3c8e8e8bf6b2815dbaf396a2d0a28fb7b
Philippe MICHEL <philippe@docteur-michel.fr>
2025-05-06 23:04:52 +0200

    Import OK

--- import.R
+++ import.R
@@ -11,11 +11,15 @@ rm(list = ls())
 #
 library(tidyverse)
 library(labelled)
+library(janitor)
 library(readODS)
 library(baseph)
+library(lubridate)
+library(explore)
+library(here)
 #
 impf <- function(noo){
-  zz <- read_ods(read_ods("datas/samutraumas.ods", sheet = noo) |>
+  zz <- read_ods("datas/samutrauma.ods", sheet = noo, na = c("","NA","ND")) |>
                    janitor::clean_names() |>
                    mutate(across(where(is.character), as.factor))
                      }
@@ -25,5 +29,21 @@ intervention <- impf("intervention")
 destination <- impf("destination")
 #
 tt <- left_join(regulation,intervention)
-tt <- left_join(tt,destination) |>
+tt <- left_join(tt,destination)
+tt <- tt |>
+  remove_constant() |>
+  mutate(horaire = hms(horaire)) |>
+  mutate(intox  = as.factor(ifelse((intox_oh == "Oui")|(intox_toxique == "Oui"), "Oui", "Non"))) |>
+  relocate(intox, .after = demande_vsav) |>
+  dplyr::select(-c(intox_oh, intox_toxique)) |>
+  mutate(across(starts_with("niveau_"), ~ as.factor(.)))
+#bn
+bn  <- read_ods("datas/samutrauma.ods", sheet = "bnom")
+var_label(tt) <- bn$nom
 #
+save(tt, bn, file = "datas/samutraumas.RData")
+
+
+
+tt %>%
+  report(output_file = "SAMU-TRAUMAS.html", output_dir = here::here())
