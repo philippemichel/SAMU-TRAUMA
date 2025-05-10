9a1939da4851180f6f38a518e77d1f69d6532c32
Philippe MICHEL <philippe@docteur-michel.fr>
2025-05-10 19:35:48 +0200

    biblio OK, tableaux OK

--- import.R
+++ import.R
@@ -22,7 +22,15 @@ impf <- function(noo){
   zz <- read_ods("datas/samutrauma.ods", sheet = noo, na = c("","NA","ND")) |>
                    janitor::clean_names() |>
                    mutate(across(where(is.character), as.factor))
-                     }
+}
+# Destination finale
+destf <- function(d1,d2){
+  dd <- d1
+  ww <- which(!is.na(d2))
+  dd[ww] <- d2[ww]
+  return(dd)
+}
+
 #
 regulation <- impf("regulation")
 intervention <- impf("intervention")
@@ -36,7 +44,30 @@ tt <- tt |>
   mutate(intox  = as.factor(ifelse((intox_oh == "Oui")|(intox_toxique == "Oui"), "Oui", "Non"))) |>
   relocate(intox, .after = demande_vsav) |>
   dplyr::select(-c(intox_oh, intox_toxique)) |>
-  mutate(across(starts_with("niveau_"), ~ as.factor(.)))
+  mutate(across(starts_with("niveau_"), ~ as.factor(.))) |>
+  mutate(shock = ifelse(shock_index > 0.899, "Positif", "Négatif")) |>
+  relocate(shock, .after = shock_index) |>
+  mutate(across(starts_with("niveau_"), ~ fct_recode(., "Niveau 1" = "1", "Niveau 3" = "3"))) |>
+  mutate(mgap = cut(mgap,
+                    include.lowest = TRUE,
+                    right = FALSE,
+                    dig.lab = 4,
+                    breaks = c(0, 18, 23, 30),
+                    labels = c("MGAP < 18", "MGAP 18-22", "MGAP > 22")
+  )) |>
+  ## Recodage de tt$triage en tt$triage_rec
+mutate(triage = fct_recode(triage,
+    "Normo-triage" = "Normo",
+    "Sous-triage" = "Sous",
+    "Sur-triage" = "Sur"
+  ))
+
+c <- destf(tt$niveau_tc_1,tt$niveau_tc_2)
+tt$nivfin<- c |>
+  fct_recode(
+    "Niveau 1" = "1",
+    "Niveau 3" = "3"
+  )
 #bn
 bn  <- read_ods("datas/samutrauma.ods", sheet = "bnom")
 var_label(tt) <- bn$nom
@@ -45,5 +76,5 @@ save(tt, bn,regulation,intervention,destination,  file = "datas/samutraumas.RDat
 
 
 
-tt %>%
-  report(output_file = "SAMU-TRAUMAS.html", output_dir = here::here())
+# tt %>%
+  #report(output_file = "SAMU-TRAUMAS.html", output_dir = here::here())
